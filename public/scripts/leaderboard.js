55b2e1b97c1941bfed983cc83e529fb844e55e88
StevenPintoVC <steven.pinto@vcontractor.co.za>
2023-06-24 15:41:40 +0200

    Leaderboard population with endpoint

--- public/scripts/leaderboard.js
+++ public/scripts/leaderboard.js
@@ -19,4 +19,30 @@ window.addEventListener('load',() =>{
   }
 });
 
+async function populateLeaderboard(){
+  try{
+    const response = await fetch('http://localhost:8080/leaderboard', {
+        method: 'GET',
+        mode: 'cors',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${sessionStorage.getItem('token')}`
+        }
+      });
+      const myJson = await response.json();
+      console.log(myJson);
+      let list = document.getElementById('leaderList');
+      for(let i =0; i< myJson.length || i<5; i++){
+        let listItem = document.createElement('li');
+        console.log(listItem);
+        listItem.innerText=`${myJson[i].username}: ${myJson[i].highscore} seconds`;
+        list.appendChild(listItem);
+      }
+  }catch(err){
+    console.log(err);
+  }
+
+}
+
 populateBestTime();
+populateLeaderboard();
